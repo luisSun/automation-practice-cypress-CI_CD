49e778b5be71338e8530c4ae066974100b6e0473
luisSun <luis.sun@gmail.com>
2025-10-19 15:39:59 -0300

    Adicionado o MochaWelsome

--- cypress.config.js
+++ cypress.config.js
@@ -4,8 +4,16 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: "https://automationpratice.com.br/",
     defaultCommandTimeout: 5000,
+    reporter: 'cypress-mochawesome-reporter',
+    reporterOptions: {
+      charts: true,
+      reportPageTitle: 'Projeto de Cypress',
+      embeddedScreenshots: true,
+      inlineAssets: true,
+      saveAllAttempts: false,
+    },
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
